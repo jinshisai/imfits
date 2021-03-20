810a17c154fcf706918b41adfe1015a1905329f0
Jinshi Sai <jinshi@Jinshis-MacBook-Pro.local>
2021-03-20 19:27:27 +0800

    Minor update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="imfits",
-	version='1.0',
+	version='1.1',
 	description='Read and handle fits files for astronomy easily.',
 	author='Jinshi Sai',
 	author_email='jn.insa.sai@gmail.com',
