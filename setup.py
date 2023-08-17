60682a83fcea65362695764c4a8e8638474a1db5
jinshisai <jn.insa.sai@gmail.com>
2023-08-17 11:22:36 +0800

    Update version to 2.1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="imfits",
-	version='2.0.1',
+	version='2.1.0',
 	description='Read and handle fits files for astronomy easily.',
 	author='Jinshi Sai',
 	author_email='jn.insa.sai@gmail.com',
