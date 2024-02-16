b8eb6ef69cbc2ed45b9cb68f2f52139669e606d6
jinshisai <jn.insa.sai@gmail.com>
2024-02-16 22:26:00 +0800

    v2.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'imfits'
 copyright = '2023, Jinshi Sai'
 author = 'Jinshi Sai'
-release = 'v2.0.1'
+release = 'v2.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
